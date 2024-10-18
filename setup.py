2dc0b1ba2559e455b50c2c50e3d77432fd6fff7c
Mohammad Nz <mn68728@gmail.com>
2024-10-18 15:57:30 +0330

    bump to version 0.2

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
 setup(name="fixer-demo",
-      version='0.1',
+      version='0.2',
       description="Fixer service demo package",
       url="#",
       author="Mohammad",
       author_email="Mohammad@gmail.com",
       license="MIT",
       packages=['fixer'],
+      install_require=["requests"],
       zip_safe=False)
