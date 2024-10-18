ad91ff5844fa550c6bcc52f5ea6111d6934c2f5f
Mohammad Nz <mn68728@gmail.com>
2024-10-18 16:11:48 +0330

    fix bug version

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(name="fixer-demo",
-      version='0.2',
+      version='0.2.1',
       description="Fixer service demo package",
       url="#",
       author="Mohammad",
       author_email="Mohammad@gmail.com",
       license="MIT",
       packages=['fixer'],
-      install_require=["requests"],
+      install_requires=["requests"],
       zip_safe=False)
